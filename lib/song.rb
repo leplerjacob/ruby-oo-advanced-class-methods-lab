fbcfea8ab5ca76cd1e21b14e1f6581f12097d738
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-18 08:33:12 -0500

    commit tree 29a3d6f084278b69a1131c7e261e8ce405f36fb8

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,42 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name = "")
+    @name = name
+    @@all << self
+  end
+
+  def self.create
+    self.new
+    @@all[-1]
+  end
+
+  def self.new_by_name(name)
+    self.new(name)
+  end
+
+  private
+  def self.create_by_name(name)
+    self.new(name)
+  end
+
+  private
+  def self.find_by_name(name)
+    @@all.select{|song| song.name == name}[0]
+  end
+
+  def self.find_or_create_by_name(name)
+    !find_by_name(name) ? create_by_name(name) : find_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    self.new(filename[/-.+/])
+  end
+
   def self.all
     @@all
   end
